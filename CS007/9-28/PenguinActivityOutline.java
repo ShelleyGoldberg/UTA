5e2b150883e2690e6bf9f0dbf4ef9b6891a94a27
Shelley Goldberg <sag109@pitt.edu>
2015-09-28 00:41:48 -0400

    added activity outline

---
+++ CS007/9-28/PenguinActivityOutline.java
@@ -0,0 +1,35 @@
+/* Make an informational comment */
+
+/* add class and main method boilerplate */
+
+/* declare and initialize variables as needed */
+
+/* initialize a Scanner */
+
+/* get the user's name & welcome them*/
+
+/* get the number of penguins the user has met */
+
+/* get the number of penguins the user has liked */
+
+/* calculate the percentage of penguins that the user has liked and tell the user */
+
+/* calculate how many penguins we expect the user to like at our zoo and tell the user */
+
+
+
+
+
+
+/*
+This program will ask a user for their name, find out how many penguins they've
+met and liked, then calculate the number of penguins at our zoo that we 
+expect the user to like. 
+
+To do this, the users name will need to be stored along with variables
+for the number of penguins met, number liked, percentage liked, number
+in our zoo, and finally number we estimate that they will like. 
+
+The number of penguins in our zoo should be hard coded, but the other values
+should be given by the user or calculated
+*/
